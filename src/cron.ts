d743d18535d60e797f4bb3ef1313d576f77a70cd
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-07-20 10:45:28 +0100

    better message

--- src/cron.ts
+++ src/cron.ts
@@ -11,14 +11,15 @@ const keep_alive = process.env.KEEP_ALIVE as string;
 server.on('request', (request, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-      data: 'Hello World!',
+      data: 'I am alive!',
     }));
   });
 
 cron.schedule("*/2 * * * *", async ()=>{
     console.log("Hello world")
     try{
-      const revive_wa_bot = await axios.get(keep_alive);
+      const revive_keep_alive = await axios.get(keep_alive);
+      console.log(`keep-alive says ${revive_keep_alive.data}`)
       // const revive_the_keeper = await axios.get(the_keeper)
     }catch (error){
       console.error(error)
